3647b00f55d5fb29aefb7842bd5f9b425f939947
iceyang <w@icey.cc>
2018-12-19 10:07:53 +0800

    add

--- src/Constant.js
+++ src/Constant.js
@@ -9,8 +9,8 @@ let Configs = {
    * dev开发
    */
   // DEV_HOST: 'https://finsuitdev.bicai365.com', // dev开发环境
-  // DEV_HOST: 'https://finsuit.bicai365.com', // 生产
-  DEV_HOST: 'https://adv.bicai365.com', // adv测试
+  DEV_HOST: 'https://finsuit.bicai365.com', // 生产
+  // DEV_HOST: 'https://adv.bicai365.com', // adv测试
   // DEV_HOST: 'https://demo1.bicai365.com', // adv测试
   // DEV_HOST:'https://graytest.bicai365.com' ,// 灰度测试
   /**
