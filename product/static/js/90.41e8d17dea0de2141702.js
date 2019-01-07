91c011f003601142d186c0776ef81f39daa1e6b3
iceyang <w@icey.cc>
2019-01-07 13:21:23 +0800

    合并新增打点

--- product/static/js/90.41e8d17dea0de2141702.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([90],{Z5Ba:function(t,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var a={render:function(){var t=this.$createElement,e=this._self._c||t;return e("div",[e("app-bar",{attrs:{title:"提现"}}),this._v("\n    等2小时\n    "),e("button",{staticClass:"begain",on:{click:this.goNext}},[this._v("确定")])],1)},staticRenderFns:[]};var i=n("VU/8")({name:"WaitForWithdraw",methods:{goNext:function(){}}},a,!1,function(t){n("dxlo")},"data-v-397d98ba",null);e.default=i.exports},dxlo:function(t,e){}});
-//# sourceMappingURL=90.41e8d17dea0de2141702.js.map
