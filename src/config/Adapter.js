009650104a6d053be59d0997c25979f1bd5566f9
iceyang <w@icey.cc>
2019-01-21 13:42:36 +0800

    暂时删除郑州行

--- src/config/Adapter.js
+++ src/config/Adapter.js
@@ -5,27 +5,29 @@ let ORG_ID = util.storage.session.get('ORG_ID') || ''
 /**
  * api
  */
-import api_ZZH from '@/api/bank/api.ZZH'
-import api_GSH from '@/api/bank/api.KSH'
+// import api_ZZH from '@/api/bank/api.ZZH'
+import api_KSH from '@/api/bank/api.KSH'
+import api_GSH from '@/api/bank/api.GSH'
 /**
  * router
  */
-import router_ZZH from '@/router/bank/router.ZZH'
-import router_GSH from '@/router/bank/router.KSH'
+// import router_ZZH from '@/router/bank/router.ZZH'
+import router_KSH from '@/router/bank/router.KSH'
+import router_GSH from '@/router/bank/router.GSH'
 
 let currentBank = {API: {}, ROUTER: {}};
 switch (ORG_ID + '') {
-  case ORG_ID_NUM.ZHENGZHOU:
+  case ORG_ID_NUM.ZZH:
     currentBank = {
       API: api_ZZH,
       ROUTER: router_ZZH
     }
     break;
 
-  case ORG_ID_NUM.KESHANG:
+  case ORG_ID_NUM.KSH:
     currentBank = {
-      API: api_KESHANG,
-      ROUTER: router_KESHANG
+      API: api_KSH,
+      ROUTER: router_KSH
     }
     break;
     case ORG_ID_NUM.GSH:
